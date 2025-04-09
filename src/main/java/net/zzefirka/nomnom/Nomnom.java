d7dd39dde743ceff2175746d0e0f2e03250dddfd
Jakob Panin <zzefirka14@gmail.com>
2025-04-09 21:09:00 +0300

    First commit

---
+++ src/main/java/net/zzefirka/nomnom/Nomnom.java
@@ -0,0 +1,17 @@
+package net.zzefirka.nomnom;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Nomnom implements ModInitializer {
+	public static final String MOD_ID = "Nomnom";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/zzefirka/nomnom/NomnomClient.java
@@ -0,0 +1,10 @@
+package net.zzefirka.nomnom;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NomnomClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
